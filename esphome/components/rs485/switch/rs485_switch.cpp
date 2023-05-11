9e73930d6b1756b5a1ee5fb64411583bc05b34e3
Sanggyun Bang <58872165+harryphiles@users.noreply.github.com>
2023-05-11 18:33:51 +0900

    Update rs485_switch.cpp

--- esphome/components/rs485/switch/rs485_switch.cpp
+++ esphome/components/rs485/switch/rs485_switch.cpp
@@ -7,12 +7,12 @@ namespace rs485 {
 static const char *TAG = "rs485.switch";
 
 void RS485Switch::dump_config() {
-    ESP_LOGCONFIG(TAG, "RS485 Switch '%s':", device_name_->c_str());
+    ESP_LOGCONFIG(TAG, "RS485 Switch '%s':", device_name_.c_str());
     dump_rs485_device_config(TAG);
 }
 
 void RS485Switch::publish(const uint8_t *data, const num_t len) {
-    ESP_LOGW(TAG, "'%s' State not found: %s", device_name_->c_str(), hexencode(&data[0], len).c_str());
+    ESP_LOGW(TAG, "'%s' State not found: %s", device_name_.c_str(), hexencode(&data[0], len).c_str());
 }
 
 }  // namespace rs485
