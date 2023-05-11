d69e2ff671995ebcd3f5382ac5403ef02d823027
Sanggyun Bang <58872165+harryphiles@users.noreply.github.com>
2023-05-11 19:52:14 +0900

    Update rs485_switch.h

--- esphome/components/rs485/switch/rs485_switch.h
+++ esphome/components/rs485/switch/rs485_switch.h
@@ -1,24 +1,23 @@
 #pragma once
 
-#include "esphome/core/component.h"
 #include "esphome/components/rs485/rs485.h"
 #include "esphome/components/switch/switch.h"
 
 namespace esphome {
 namespace rs485 {
 
-class RS485Switch : public switch::Switch, public RS485Device {
+class RS485Switch : public switch_::Switch, public RS485Device {
   public:
-    RS485Switch() { this->device_name_ = &this->name_.c_str(); }
+    RS485Switch() { device_name_ = &this->name_; }
     void dump_config() override;
     void publish(const uint8_t *data, const num_t len) override;
     bool publish(bool state) override { publish_state(state); return true; }
 
     void write_state(bool state) override {
-    if(state == this->state) return;
-    
-    write_with_header(state ? this->get_command_on() : this->get_command_off());
-    publish_state(state);
+        if(state == this->state) return;
+      
+        write_with_header(state ? this->get_command_on() : this->get_command_off());
+        publish_state(state);
     }
 
   protected:
