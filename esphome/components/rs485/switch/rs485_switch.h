e45dd328e4c9e1bf9cac3a31c9b2bc73ec345ace
Sanggyun Bang <58872165+harryphiles@users.noreply.github.com>
2023-05-11 19:08:02 +0900

    Update rs485_switch.h

--- esphome/components/rs485/switch/rs485_switch.h
+++ esphome/components/rs485/switch/rs485_switch.h
@@ -8,11 +8,11 @@ namespace rs485 {
 
 class RS485Switch : public switch_::Switch, public RS485Device {
   public:
-    RS485Switch() { device_name_ = &this->name_; }
+    RS485Switch() { this->device_name_ = &this->name_; }
     void dump_config() override;
     void publish(const uint8_t *data, const num_t len) override;
     bool publish(bool state) override { publish_state(state); return true; }
-    
+
     void write_state(bool state) override {
     if(state == this->state) return;
     
